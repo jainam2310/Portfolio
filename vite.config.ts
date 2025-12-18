faab3553129fe02e3d529832b27a04013dea83d1
jainam2310 <jainam23mehta@gmail.com>
2025-12-18 10:51:46 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "./",
-});
+  base: '/Portfolio/', // Your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
